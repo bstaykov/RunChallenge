0c7daad6a3d46a9ac5f2cc44f9a6db7f9311bfb3
bstaykov <boycho.staykov@gmail.com>
2014-11-05 18:23:35 +0200

    MyLastWorkouts

--- RunChallenge.MVC/Controllers/HomeController.cs
+++ RunChallenge.MVC/Controllers/HomeController.cs
@@ -10,19 +10,37 @@ namespace RunChallenge.MVC.Controllers
     [OutputCache(Duration = 10)]
     public class HomeController : Controller
     {
-        RunChallengeDbContext dbContext;
+        IRunChallengeData data;
 
         public HomeController()
+            :this(new RunChallengeData(new RunChallengeDbContext()))
         {
-            this.dbContext = new RunChallengeDbContext();
         }
+
+        public HomeController(IRunChallengeData data)
+        {
+            this.data = data;
+        }
+
+        //RunChallengeDbContext dbContext;
+
+        //public HomeController()
+        //{
+        //    this.dbContext = new RunChallengeDbContext();
+        //}
 
         public ActionResult Index()
         {
-            var lastWorkouts = this.dbContext.Workouts
+            var lastWorkouts = this.data.Workouts.All()
+                .Where(d => d.Date < DateTime.Now)
                 .OrderByDescending(w => w.Date)
                 .Take(3);
             ViewData["LastWorkouts"] = lastWorkouts;
+            
+            //var lastWorkouts = this.dbContext.Workouts
+            //    .OrderByDescending(w => w.Date)
+            //    .Take(3);
+            //ViewData["LastWorkouts"] = lastWorkouts;
 
             return View();
         }
--- RunChallenge.MVC/Controllers/WorkoutController.cs
+++ RunChallenge.MVC/Controllers/WorkoutController.cs
@@ -4,22 +4,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+//using Microsoft.AspNet.Identity.IdentityExtensions;
+
 
 namespace RunChallenge.MVC.Controllers
 {
     public class WorkoutController : Controller
     {
-        RunChallengeDbContext dbContext;
+        IRunChallengeData data;
 
         public WorkoutController()
+            :this(new RunChallengeData(new RunChallengeDbContext()))
         {
-            this.dbContext = new RunChallengeDbContext();
+        }
+
+        public WorkoutController(IRunChallengeData data)
+        {
+            this.data = data;
         }
 
         // GET: Workout
         public ActionResult Index()
         {
-            var lastWorkouts = this.dbContext.Workouts
+            var currentUserId = this.User.Identity.GetUserId();
+            var lastWorkouts = this.data.Workouts.All()
+                .Where(u => u.UserId == currentUserId && u.Date < DateTime.Now)
                 .OrderByDescending(w => w.Date)
                 .Take(3);
             ViewData["LastWorkouts"] = lastWorkouts;
